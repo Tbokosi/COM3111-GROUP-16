1bd8a9a6188f25ca21d56ee6adb3272e4abc14e2
MichaelMakawa <bsc-com-05-22@unima.ac.mw>
2025-10-18 19:59:05 +0200

    header

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+import { Header } from "../components/Header";
+import { ProductComponent } from "../components/Product";
+
+<div className="ProductContainer">
+    <div className="product1 bg-green-500 p-4">ProductComponent</div>
+    <div className="product2 bg-red-500 p-4"></div>
+    <div className="product3 bg-yellow-500 p-4"></div> 
+</div>
